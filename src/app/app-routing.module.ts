83af075d376cb33f23283b0cfd5f4fcbcbe4d446
Ralph Martin Flores <201811044@gordoncollege.edu.ph>
2021-03-27 14:08:10 +0800

    Initial architectural design for login and home page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -11,6 +11,14 @@ const routes: Routes = [
     redirectTo: 'home',
     pathMatch: 'full'
   },
+  {
+    path: 'login',
+    loadChildren: () => import('./login/login.module').then( m => m.LoginPageModule)
+  },
+  {
+    path: 'products',
+    loadChildren: () => import('./products/products.module').then( m => m.ProductsPageModule)
+  },
 ];
 
 @NgModule({
