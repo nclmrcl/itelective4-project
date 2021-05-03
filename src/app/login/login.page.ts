853201f128c4ad87537ea92f6e7a8e35e1d5c4c6
NclMrcl <nclmrclxx@gmail.com>
2021-05-04 04:18:18 +0800

    Login Alert Toast

--- src/app/login/login.page.ts
+++ src/app/login/login.page.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { DataService } from '../services/data.service';
 import { UserService } from '../services/user.service';
+import { ToastController} from '@ionic/angular';
 
 @Component({
   selector: 'app-login',
@@ -10,7 +11,7 @@ import { UserService } from '../services/user.service';
 })
 export class LoginPage implements OnInit {
 
-  constructor(private ds: DataService, private user: UserService, private router: Router) { }
+  constructor(private ds: DataService, private user: UserService, private router: Router, private toastCtrl: ToastController) { }
 
   passwordType: string = 'password';
   passwordIcon: string = 'eye';
@@ -25,18 +26,40 @@ export class LoginPage implements OnInit {
   acc_info: any = {};
 
   login() {
+    
     this.acc_info.acc_password = this.acc_password;
     this.acc_info.acc_username = this.acc_username;
+
+    if(this.acc_info.acc_password == null || this.acc_info.acc_username ==null){
+      this.presentToast('Invalid Username/Password!','');
+      return;
+    }
 
     this.ds.sendApiRequest("login/", this.acc_info).subscribe((data: { payload: any[]; }) => {
       this.dt = data.payload;
       window.sessionStorage.setItem(btoa('payload'), this.dt);
       this.user.setLogin();
       this.router.navigate(['/products']);
+      
+    }, er => {
+      this.presentToast('Invalid Username/Password!', 'The Username/Password you entered is incorrect');
     });
   }
+
   hideShowPassword() {
     this.passwordType = this.passwordType === 'password' ? 'text' : 'password';
     this.passwordIcon = this.passwordIcon === 'eye' ? 'eye-off' : 'eye';
   }
+
+  async presentToast(messageError, headerError) {
+    const toast = await this.toastCtrl.create({
+        duration: 800,
+        color: 'danger',
+        message: messageError,
+        position: 'bottom',
+        cssClass: 'my-custom-class'
+      });
+    toast.present();
+  }
+
 }
--- src/app/products/products.page.ts
+++ src/app/products/products.page.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { DataService } from '../services/data.service';
+import { ToastController} from '@ionic/angular';
 
 @Component({
   selector: 'app-products',
@@ -9,10 +10,11 @@ import { DataService } from '../services/data.service';
 })
 export class ProductsPage implements OnInit {
 
-  constructor(private router: Router, private ds: DataService) { }
+  constructor(private router: Router, private ds: DataService, private toastCtrl: ToastController) { }
 
   ngOnInit() {
     this.getUserProfile();
+
   }
 
   category(cat: string) {
@@ -25,7 +27,19 @@ export class ProductsPage implements OnInit {
     let pload = JSON.parse(atob(window.sessionStorage.getItem(btoa('payload'))));
     this.ds.sendApiRequest("accounts/" + pload.id, null).subscribe((data: { payload: any[]; }) => {
       this.dt = data.payload;
+      this.welcomeToast("Welcome, " + this.dt[0].acc_fname + " " + this.dt[0].acc_lname + "!");
       console.log(this.dt)
     });
+  }
+
+  async welcomeToast(messageSuccess) {
+    const toast = await this.toastCtrl.create({
+        duration: 1000,
+        color: 'dark',
+        message: messageSuccess,
+        position: 'bottom',
+        cssClass: 'my-custom-class',
+      });
+    toast.present();
   }
 }
