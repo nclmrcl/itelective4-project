f7b4bbe36bdf82e748f3a81cb6d4f710f08993e0
NclMrcl <nclmrclxx@gmail.com>
2021-05-17 01:28:52 +0800

    myorders status

--- src/app/productdescription/productdescription.page.ts
+++ src/app/productdescription/productdescription.page.ts
@@ -12,6 +12,8 @@ export class ProductdescriptionPage implements OnInit {
 
   constructor(private ds: DataService, private router: Router, private toastCtrl: ToastController, route:ActivatedRoute) {route.params.subscribe(val => {
     this.productDetails();
+    this.getCart();
+    console.log("Hello")
   });}
 
   ngOnInit() {
@@ -41,19 +43,32 @@ export class ProductdescriptionPage implements OnInit {
   }
 
   addToCart(id) {
+
     let pload = JSON.parse(atob(window.sessionStorage.getItem(btoa('payload'))));
     console.log(this.dt);
     this.cartContent.acc_id = pload.id;
     this.cartContent.product_id = id;
-    
-    console.log(this.cart_content[0].product_id)
-    for(var i = 0; i < this.cart_content.length; i++) {
-      if(this.cart_content[i].product_id == id) {
-        console.log("product already in cart");
-      } else {
-        this.ds.sendApiRequest("addToCart/", this.cartContent).subscribe((data: { payload: any[]; }) => {
-          this.successToast( this.dt[0].product_name + " successfully added to your cart");
-        });
+    console.log(this.cart_content.length)
+    this.getCart();
+    if(this.cart_content.length == 0){
+      this.ds.sendApiRequest("addToCart/", this.cartContent).subscribe((data: { payload: any[]; }) => {
+        this.successToast( this.dt[0].product_name + " successfully added to your cart");
+        this.getCart();
+  
+      });
+      console.log(this.cart_content.length)
+    }else{
+      for(var i = 0; i < this.cart_content.length; i++) {
+        
+        console.log(this.cart_content[i].product_id, id)
+        if(this.cart_content[i].product_id == id) {
+          this.presentToast( this.dt[0].product_name + " is already in the cart", '');
+        } else {
+          this.ds.sendApiRequest("addToCart/", this.cartContent).subscribe((data: { payload: any[]; }) => {
+            this.successToast( this.dt[0].product_name + " successfully added to your cart");
+          });
+        }
+        break;
       }
     }
   }
@@ -65,6 +80,18 @@ export class ProductdescriptionPage implements OnInit {
         message: messageSuccess,
         position: 'bottom',
         cssClass: 'my-custom-class',
+      });
+    toast.present();
+  }
+
+  
+  async presentToast(messageError, headerError) {
+    const toast = await this.toastCtrl.create({
+        duration: 800,
+        color: 'danger',
+        message: messageError,
+        position: 'bottom',
+        cssClass: 'my-custom-class'
       });
     toast.present();
   }
