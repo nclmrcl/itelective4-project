b08c6e8234a508cf6298e268ebfbbc9e297edc54
Ralph Martin Flores <201811044@gordoncollege.edu.ph>
2021-05-20 02:22:25 +0800

    MyOrders bugfix and minor UI improvements

--- src/app/myorders/myorders.page.ts
+++ src/app/myorders/myorders.page.ts
@@ -13,13 +13,17 @@ export class MyordersPage {
 
   constructor(private ds: DataService, private router: Router) { }
 
-  ionViewWillEnter() {
+  ionViewDidEnter() {
     //Set first tab as default selected segment
     this.type = "pendingOrders";
     this.myOrders();
   }
 
   dt: any[] = [];
+  pending: any;
+  tbd: any;
+  delivered: any;
+  cancelled: any;
 
   myOrders() {
     let pload = JSON.parse(atob(window.sessionStorage.getItem(btoa('payload'))));
@@ -27,6 +31,19 @@ export class MyordersPage {
     this.ds.sendApiRequest("order/" + pload.id, null).subscribe((data: { payload: any[]; }) => {
       this.dt = data.payload;
       console.log(this.dt)
+      
+      this.pending = this.dt.filter(t=>t.order_status == 1);
+      console.log('Pending:', this.pending);
+
+      this.tbd = this.dt.filter(t=>t.order_status == 2);
+      console.log('TBD:', this.pending);
+
+      this.delivered = this.dt.filter(t=>t.order_status == 3);
+      console.log('Delivered:', this.pending);
+
+      this.cancelled = this.dt.filter(t=>t.order_status == 0);
+      console.log('Cancelled:', this.pending);
     });
+
   }
 }
