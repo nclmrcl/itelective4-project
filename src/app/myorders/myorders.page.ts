56fe79872b1c465ea8a3fe3872ad1e6f323d7e6a
NclMrcl <nclmrclxx@gmail.com>
2021-05-20 11:07:36 +0800

    myorders filter minor revision

--- src/app/myorders/myorders.page.ts
+++ src/app/myorders/myorders.page.ts
@@ -21,9 +21,7 @@ export class MyordersPage {
 
   dt: any[] = [];
   pending: any;
-  tbd: any;
-  delivered: any;
-  cancelled: any;
+  completed: any;
 
   myOrders() {
     let pload = JSON.parse(atob(window.sessionStorage.getItem(btoa('payload'))));
@@ -32,17 +30,11 @@ export class MyordersPage {
       this.dt = data.payload;
       console.log(this.dt)
       
-      this.pending = this.dt.filter(t=>t.order_status == 1);
+      this.pending = this.dt.filter(t=>t.order_status == 1 || t.order_status == 2);
       console.log('Pending:', this.pending);
 
-      this.tbd = this.dt.filter(t=>t.order_status == 2);
-      console.log('TBD:', this.pending);
-
-      this.delivered = this.dt.filter(t=>t.order_status == 3);
-      console.log('Delivered:', this.pending);
-
-      this.cancelled = this.dt.filter(t=>t.order_status == 0);
-      console.log('Cancelled:', this.pending);
+      this.completed = this.dt.filter(t=>t.order_status == 3 || t.order_status == 0);
+      console.log('Completed:', this.completed);
     });
 
   }
